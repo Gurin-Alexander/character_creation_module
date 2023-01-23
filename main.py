6cf1fb74d5f0e4107843d36d9c0991757b14ad1d
Гурин Александр <ALPHAtm@yandex.ru>
2023-01-24 00:22:11 +0300

    code according to PEP8

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
 
+
+# Вычисляет нанесеный персонажем урон.
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
@@ -14,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
     return ('Вы промахнулись')
 
 
+# Вычисляет количество урона заблокированного персонажем.
 def defence(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
@@ -24,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
     return ('Вы промахнулись')
 
 
+# Вычисляет эффект от использования специального умения персонажа.
 def special(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
@@ -35,6 +40,7 @@ def special(char_name: str, char_class: str) -> str:
     return ('Вы промахнулись')
 
 
+# Управление действиями персонажа.
 def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
@@ -59,6 +65,7 @@ def start_training(char_name: str, char_class: str) -> str:
     return 'Тренировка окончена.'
 
 
+# Предлагает выбрать персонажа.
 def choice_char_class() -> str:
     approve_choice: str = ''
     char_class: str = ''
@@ -82,7 +89,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -92,6 +100,3 @@ def main() -> str:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
